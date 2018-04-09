55623aa36310cd30e41913be4564eb8a3a945c7d
Caresa <caresa.huddleston@gmail.com>
2018-04-09 18:36:41 -0500

    refactor code 1

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const questions = [
 		'Moving Panoramas',	
 		'Thee Headcoatees'
 	],
-	correctAnswer: 3,
+	correctIndex: 3,
 	answersImg: '<img src="./imageFiles/question1.jpg">'
 },
 {
@@ -17,92 +17,106 @@ const questions = [
 	answers: [
 		'Frank Black',
 		'Black Francis',
-		'Charles Michael Kittridge Thompson IV',
+		'Charles Michael Kittridge Thompson IV',
 		'Dave Philips'
 	],
-	correctAnswer: 2,
+	correctIndex: 2,
 	answersImg: '<img src="./imageFiles/question2.jpg">'
 },
 {
 	questionNumber: 3,
 	question: "On the Sex Pistols studio album ‘Never Mind the Bollocks, Here's the Sex Pistols’ what was the only song that Sid Vicious recorded?",
-	answerOne: 'Bodies',
-	answerTwo: 'God Save the Queen',
-	answerThree: 'Pretty Vacant',
-	answerFour: 'Holidays in the Sun',
-	correctAnswer: 'Holidays in the Sun',
+	answers: [
+		'Bodies',
+		'God Save the Queen',
+		'Pretty Vacant',
+		'Holidays in the Sun'
+	],
+	correctIndex: 3,
 	answersImg: '<img src="./imageFiles/question3.jpg">'
 },
 {
 	questionNumber: 4,
 	question: "In what city was the video for The Clash song ‘Rock the Casbah’ recorded?",
-	answerOne: "New York",
-	answerTwo: "Chicago",
-	answerThree: "London",
-	answerFour: "Austin",
-	correctAnswer: "Austin",
+	answers: [
+		"New York",
+		"Chicago",
+		"London",
+		"Austin"
+	],
+	correctIndex: 3,
 	answersImg: '<img src="./imageFiles/question4.jpg">'
 },
 {
 	questionNumber: 5,
 	question: "What influential 70s punk band recently toured for their 40 year anniversary?",
-	answerOne: "The Damned",
-	answerTwo: "Circle Jerks",
-	answerThree: "X",
-	answerFour: "Bad Brains",
-	correctAnswer: "X",
+	answers: [
+		"The Damned",
+		"Circle Jerks",
+		"X",
+		"Bad Brains"
+	],
+	correctIndex: 2,
 	answersImg: '<img src="./imageFiles/question5.jpg">'
 },
 {
 	questionNumber: 6,
 	question: "What was the first band that Nick Cave was the frontman for?",
-	answerOne: "Nick Cave and the Bad Seeds",
-	answerTwo: "The Birthday Party",
-	answerThree: "Grinderman",
-	answerFour: "Nick Cave",
-	correctAnswer: "The Birthday Party",
+	answers: [
+		"Nick Cave and the Bad Seeds",
+		"The Birthday Party",
+		"Grinderman",
+		"Nick Cave"
+	],
+	correctIndex: 1,
 	answersImg: '<img src="./imageFiles/question6.jpg">'
 },
 {
 	questionNumber: 7,
 	question: "What is the name of the lead singer of the band The Dead Boys?",
-	answerOne: "Cheetah Chrome",
-	answerTwo: "Jonny Blitz",
-	answerThree: "Jimmy Zero",
-	answerFour: "Gene O’Connor",
-	correctAnswer: "Cheetah Chrome",
+	answers: [
+		"Cheetah Chrome",
+		"Jonny Blitz",
+		"Jimmy Zero",
+		"Gene O’Connor"
+	],
+	correctIndex: 0,
 	answersImg: '<img src="./imageFiles/question7.jpg">'
 },
 {
 	questionNumber: 8,
 	question: "What year did Clash frontman Joe Strummer die?",
-	answerOne: "2002",
-	answerTwo: "2012",
-	answerThree: "2008",
-	answerFour: "2004",
-	correctAnswer: "2002",
+	answers: [
+		"2002",
+		"2012",
+		"2008",
+		"2004"
+	],
+	correctIndex: 0,
 	answersImg: '<img src="./imageFiles/question8.jpg">'
 },
 {
 	questionNumber: 9, 
 	question: "What was the title of the Damned’s first album?",
-	answerOne: "Damned Damned Damned",
-	answerTwo: "Strawberries",
-	answerThree: "The Black Album",
-	answerFour: "Music for Pleasure",
-	correctAnswer: "Damned Damned Damned",
+	answers: [
+		"Damned Damned Damned",
+		"Strawberries",
+		"The Black Album",
+		"Music for Pleasure"
+	],
+	correctIndex: 0,
 	answersImg: '<img src="./imageFiles/question9.jpg">'
 },
 {
 	questionNumber: 10,
 	question: "What is the name of Leonard Cohen’s last album?",
-	answerOne: "Songs of Love and Hate",
-	answerTwo: "Popular Problems",
-	answerThree: "Songs from a Room",
-	answerFour: "You Want it Darker",
-	correctAnswer: "You Want it Darker",
+	answers: [
+		"Songs of Love and Hate",
+		"Popular Problems",
+		"Songs from a Room",
+		"You Want it Darker"
+	],
+	correctIndex: 3,
 	answersImg: '<img src="./imageFiles/question10.jpg">'
 }
 ]
-
-/*add answers here in nested array*/ 
--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const question = document.getElementById('js-question');
 const displayAnswer = document.getElementById('js-answer');
 const answer = document.getElementById('answer');
 const increment = document.getElementById('js-counter');
-const quizNumber = document.getElementById("js-quizNumber");
-const quizCounter = document.getElementById("js-quizCounter");
-const nextButton = document.getElementById("next");
-const startButton = document.getElementById("nav-forward");
-const finalScreen = document.getElementById("final-screen");
-let alertText = document.getElementById("dialog");
+const quizNumber = document.getElementById('js-quizNumber');
+const quizCounter = document.getElementById('js-quizCounter');
+const nextButton = document.getElementById('next');
+const startButton = document.getElementById('nav-forward');
+const finalScreen = document.getElementById('final-screen');
+let alertText = document.getElementById('dialog');
 let questionNumber = 0;
 let correct = 0;
 let wrong = 0;
@@ -23,79 +23,76 @@ $(nextButton).hide();
 $(alertText).hide();
 $(finalScreen).hide();
 
+
 //hide question#/question# on answer screen
 //final screen print out totals
 
 
-$("#nav-forward").on('click', function(event){
+$('#nav-forward').on('click', function(event){
 	if ($(this).hasClass('js-start')) {
 		startQuiz(this);
 		displayQuestion(questionNumber);
 		$(quizNumber).show();
 		$(quizCounter).show();
 	} else if($(this).hasClass('js-next')) {
+		const correctIndex = questions[questionNumber].correctIndex;
+		const correctAnswer = questions[questionNumber].answers[correctIndex];
 		let playerAnswer = $('input[name=question-radio]:checked').val();
 		$('input[type=radio]').prop('checked', false);
-
-		console.log(typeof playerAnswer);
-		console.log(playerAnswer);
-
 		if (typeof playerAnswer !== 'undefined') {
-			const correctAnswer = questions[questionNumber].correctAnswer;
-			//let answerImg = `<img src="./imageFiles/question1.jpg>" alt= "${correctAnswer}">`;
+
+			
 			let answerText;
 			let answerImg = questions[questionNumber].answersImg;
 			$(alertText).hide();
 			$(question).hide();
+		if (correctAnswer === playerAnswer){
+			answerText= `correct! the answer is ${correctAnswer}`;
+			correct ++;
 
-			if (correctAnswer === playerAnswer){
-				answerText= `correct! the answer is ${correctAnswer}`;
-				correct ++;
+			//make div for number of correct answers
+		}
+		else{
+			answerText= `sorry! the answer is ${correctAnswer}`;			
+		}
 
-				//make div for number of correct answers
-			}
-			else{
-				answerText= `sorry! the answer is ${correctAnswer}`;			
-			}
+		questionNumber ++;
+		keepScore(correct);
+		if( questionNumber <= 9 ){
+			$(displayAnswer).html(`${answerText} ${answerImg}`)
+				//$(question).hide();
+			$(displayAnswer).show();
+			$(nextButton).show();
+			$(startButton).hide();
+			displayQuestion(questionNumber);
 
-			questionNumber ++;
-			keepScore(correct);
-			if( questionNumber <= 9 ){
-				$(displayAnswer).html(`${answerText} ${answerImg}`)
-					//$(question).hide();
-				$(displayAnswer).show();
-				$(nextButton).show();
-				$(startButton).hide();
-				displayQuestion(questionNumber);
-
-			
-			}else{
-				$(finalScreen).show();
-				$(quizNumber).hide();
-				$(quizCounter).hide();
-				$(nextButton).hide();
-				$(alertText).hide();
-				$(question).hide();
-				$(startButton).html('Retake Quiz');
-				finishQuiz(quizCounter);
-				
-				//reload page
-				$(startButton).unbind().on("click", function(event){
-					
-				window.location.reload(true);
-				});
-			}
-				
-		} else {	
-			$(alertText).show();
-			console.log(questionNumber);
-			}
 		
-		}	
+		}else{
+			$(finalScreen).show();
+			$(quizNumber).hide();
+			$(quizCounter).hide();
+			$(nextButton).hide();
+			$(alertText).hide();
+			$(question).hide();
+			$(startButton).html('Retake Quiz');
+			finishQuiz(quizCounter);
+			
+			//reload page
+			$(startButton).unbind().on('click', function(event){
+				
+			window.location.reload(true);
+			});
+		}
+			
+	} else {	
+		$(alertText).show();
+		}
+	
+	}	
 	
 });
 
-$("#next").on('click', function(event) {
+$('#next').on('click', function(event) {
 	$('.showQuestion').detach();
 	displayQuestion(questionNumber);
 
@@ -108,7 +105,7 @@ $("#next").on('click', function(event) {
 
 function startQuiz (button) {
     img.style.display = 'none';
-    question.style.display = "block";
+    question.style.display = 'block';
     $(button).removeClass('js-start');
     $(button).addClass('js-next');
     $(button).text('Submit');
@@ -134,17 +131,19 @@ function showNext (button) {
 }
 
 function displayQuestion(questionNumber) {
-	console.log(questionNumber);
-	$(quizNumber).text((questionNumber+1)+'/'+questions.length);
+	$(quizNumber).text((questionNumber)+'/'+questions.length);
 	const quizQuestion = questions[questionNumber];
+
 	quizQuestion.answers.forEach(function (answer, i){
 		const choice = 
-		`<p class="showQuestion"><input type="radio" id="radio-0${i+1}" name="question-radio" value="${answer}">
-		<label for="radio-0${i+1}" id="question-0${i+1}">${answer}</label></p>`
+		`<p class='showQuestion'><input type='radio' id='radio-0${i+1}' name='question-radio' value='${answer}'>
+		<label for='radio-0${i+1}' id='question-0${i+1}'>${answer}</label></p>`
 		
 		//$(quizQuestion).append($(choice);
 		//$('.question').appendTo(choice);
 		$('#js-question').append(choice);
+		//console.log(quizQuestion[i].correctAnswer);
+		
 	});
 	$('.question-number').text(quizQuestion.questionNumber);
 	$('.question-text').text(quizQuestion.question);
@@ -157,12 +156,10 @@ function displayQuestion(questionNumber) {
 function keepScore(correct){
 	$(quizCounter).text(correct);
 	
-	console.log(questions.length)
-	
 }
 
 function finishQuiz(quizCounter) {
-	console.log(quizCounter);
+	//console.log(quizCounter);
 	$(finalScreen).append(`You got ${correct} out of 10`);
 
 }
